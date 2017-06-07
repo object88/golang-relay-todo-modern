d9fc2f92654cf48e684ea259428500c1ca862108
object88 <object88@gmail.com>
2017-06-06 20:55:14 -0600

    First push

---
+++ js/components/TodoApp.js
@@ -0,0 +1,71 @@
+import AddTodoMutation from '../mutations/AddTodoMutation';
+import TodoList from './TodoList';
+import TodoListFooter from './TodoListFooter';
+import TodoTextInput from './TodoTextInput';
+
+import React from 'react';
+import {
+  createFragmentContainer,
+  graphql,
+} from 'react-relay';
+
+class TodoApp extends React.Component {
+  _handleTextInputSave = (text) => {
+    AddTodoMutation.commit(
+      this.props.relay.environment,
+      text,
+      this.props.viewer,
+    );
+  };
+  render() {
+    const hasTodos = this.props.viewer.totalCount > 0;
+    return (
+      <div>
+        <section className="todoapp">
+          <header className="header">
+            <h1>
+              todos
+            </h1>
+            <TodoTextInput
+              autoFocus={true}
+              className="new-todo"
+              onSave={this._handleTextInputSave}
+              placeholder="What needs to be done?"
+            />
+          </header>
+          <TodoList viewer={this.props.viewer} />
+          {hasTodos &&
+            <TodoListFooter
+              todos={this.props.viewer.todos}
+              viewer={this.props.viewer}
+            />
+          }
+        </section>
+        <footer className="info">
+          <p>
+            Double-click to edit a todo
+          </p>
+          <p>
+            Created by the <a href="https://facebook.github.io/relay/">
+              Relay team
+            </a>
+          </p>
+          <p>
+            Part of <a href="http://todomvc.com">TodoMVC</a>
+          </p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default createFragmentContainer(TodoApp, {
+  viewer: graphql`
+    fragment TodoApp_viewer on User {
+      id,
+      totalCount,
+      ...TodoListFooter_viewer,
+      ...TodoList_viewer,
+    }
+  `,
+});
---
+++ js/components/TodoListFooter.js
@@ -0,0 +1,57 @@
+import RemoveCompletedTodosMutation from '../mutations/RemoveCompletedTodosMutation';
+
+import React from 'react';
+import {
+  graphql,
+  createFragmentContainer,
+} from 'react-relay';
+
+class TodoListFooter extends React.Component {
+  _handleRemoveCompletedTodosClick = () => {
+    RemoveCompletedTodosMutation.commit(
+      this.props.relay.environment,
+      this.props.viewer.completedTodos,
+      this.props.viewer,
+    );
+  };
+  render() {
+    const numCompletedTodos = this.props.viewer.completedCount;
+    const numRemainingTodos = this.props.viewer.totalCount - numCompletedTodos;
+    return (
+      <footer className="footer">
+        <span className="todo-count">
+          <strong>{numRemainingTodos}</strong> item{numRemainingTodos === 1 ? '' : 's'} left
+        </span>
+        {numCompletedTodos > 0 &&
+          <button
+            className="clear-completed"
+            onClick={this._handleRemoveCompletedTodosClick}>
+            Clear completed
+          </button>
+        }
+      </footer>
+    );
+  }
+}
+
+export default createFragmentContainer(
+  TodoListFooter,
+  graphql`
+    fragment TodoListFooter_viewer on User {
+      id,
+      completedCount,
+      completedTodos: todos(
+        status: "completed",
+        first: 2147483647  # max GraphQLInt
+      ) {
+        edges {
+          node {
+            id
+            complete
+          }
+        }
+      },
+      totalCount,
+    }
+  `
+);
