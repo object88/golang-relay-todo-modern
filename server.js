400d5b1bc5411d4761b96a581e59ce049e014ed1
object88 <object88@gmail.com>
2017-06-06 21:26:44 -0600

    Added mutations

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import graphQLHTTP from 'express-graphql';
+import path from 'path';
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import {schema} from './data/schema';
+
+const APP_PORT = 3000;
+const GRAPHQL_PORT = 8080;
+
+// Expose a GraphQL endpoint
+const graphQLServer = express();
+graphQLServer.use('/', graphQLHTTP({schema, pretty: true}));
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(
+  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
+));
+
+// Serve the Relay app
+const compiler = webpack({
+  entry: ['whatwg-fetch', path.resolve(__dirname, 'js', 'app.js')],
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        test: /\.js$/,
+      },
+    ],
+  },
+  output: {filename: 'app.js', path: '/'},
+});
+const app = new WebpackDevServer(compiler, {
+  contentBase: '/public/',
+  proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
+  publicPath: '/js/',
+  stats: {colors: true},
+});
+// Serve static resources
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.listen(APP_PORT, () => {
+  console.log(`App is now running on http://localhost:${APP_PORT}`);
+});
